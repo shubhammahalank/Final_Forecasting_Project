8fb8d00d8625e46016db3f596127e1c429bc10ed
Shubham Mahalank <shubhammahalank@gmail.com>
2020-05-05 15:07:58 -0400

    05052020-2

--- final_project.py
+++ final_project.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 import tqdm as tqdm
 import statsmodels.api as sm
-from fbprophet import Prophet
+# from fbprophet import Prophet
 
 # Importing Dataframes
 @st.cache
@@ -103,7 +103,7 @@ if choice == "State":
 	st.write("Validation period should be exactly equal to 30 days")
 	lower_val = str(st.date_input("Enter lower validation date",datetime.date(2016,3,26)))
 	upper_val = str(st.date_input("Enter upper validation date",datetime.date(2016,4,24)))
-	algo_selected = st.radio("Select Algorithm for forecasting:",("Moving Average","ARIMA","Prophet"))
+	algo_selected = st.radio("Select Algorithm for forecasting:",("Moving Average","ARIMA"))
 	date_range = pd.date_range(start='1/29/2011', end='4/24/2016').astype(str)
 
 	if st.button("SUBMIT"):
@@ -143,19 +143,19 @@ if choice == "State":
 					predictions = np.array(predictions).reshape((-1, 30))
 					error_arima = np.linalg.norm(predictions[:3] - val_dataset.values[:3])/len(predictions[0])
 
-				if algo_selected == "Prophet":
-					dates = ["2007-12-" + str(i) for i in range(1, 31)]
-					predictions = []
-					for row in tqdm.tqdm(train_dataset[train_dataset.columns[-30:]].values[:3]):
-						df_prophet = pd.DataFrame(np.transpose([dates, row]))
-						df_prophet.columns = ["ds", "y"]
-						model = Prophet(daily_seasonality=True)
-						model.fit(df_prophet)
-						future = model.make_future_dataframe(periods=30)
-						forecast = model.predict(future)["yhat"].loc[30:].values
-						predictions.append(forecast)
-					predictions = np.array(predictions).reshape((-1, 30))
-					error_prophet = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
+				# if algo_selected == "Prophet":
+				# 	dates = ["2007-12-" + str(i) for i in range(1, 31)]
+				# 	predictions = []
+				# 	for row in tqdm.tqdm(train_dataset[train_dataset.columns[-30:]].values[:3]):
+				# 		df_prophet = pd.DataFrame(np.transpose([dates, row]))
+				# 		df_prophet.columns = ["ds", "y"]
+				# 		model = Prophet(daily_seasonality=True)
+				# 		model.fit(df_prophet)
+				# 		future = model.make_future_dataframe(periods=30)
+				# 		forecast = model.predict(future)["yhat"].loc[30:].values
+				# 		predictions.append(forecast)
+				# 	predictions = np.array(predictions).reshape((-1, 30))
+				# 	error_prophet = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
 
 				fig = make_subplots(rows=1, cols=1)
 				fig.add_trace(go.Scatter(x=np.arange(70), mode='lines', y=train_dataset.loc[mean_index].values, marker=dict(color="red"),name="Trained signal"),row=1, col=1)
@@ -198,19 +198,19 @@ if choice == "State":
 					predictions = np.array(predictions).reshape((-1, 30))
 					error_arima = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
 
-				if algo_selected == "Prophet":
-					dates = ["2007-12-" + str(i) for i in range(1, 31)]
-					predictions = []
-					for row in tqdm.tqdm(train_dataset[train_dataset.columns[-30:]].values[:3]):
-						df_prophet = pd.DataFrame(np.transpose([dates, row]))
-						df_prophet.columns = ["ds", "y"]
-						model = Prophet(daily_seasonality=True)
-						model.fit(df_prophet)
-						future = model.make_future_dataframe(periods=30)
-						forecast = model.predict(future)["yhat"].loc[30:].values
-						predictions.append(forecast)
-					predictions = np.array(predictions).reshape((-1, 30))
-					error_prophet = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
+				# if algo_selected == "Prophet":
+				# 	dates = ["2007-12-" + str(i) for i in range(1, 31)]
+				# 	predictions = []
+				# 	for row in tqdm.tqdm(train_dataset[train_dataset.columns[-30:]].values[:3]):
+				# 		df_prophet = pd.DataFrame(np.transpose([dates, row]))
+				# 		df_prophet.columns = ["ds", "y"]
+				# 		model = Prophet(daily_seasonality=True)
+				# 		model.fit(df_prophet)
+				# 		future = model.make_future_dataframe(periods=30)
+				# 		forecast = model.predict(future)["yhat"].loc[30:].values
+				# 		predictions.append(forecast)
+				# 	predictions = np.array(predictions).reshape((-1, 30))
+				# 	error_prophet = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
 
 				fig = make_subplots(rows=len(item_id), cols=1)
 				for i in range(len(item_id)):
@@ -330,19 +330,19 @@ if choice == "Category":
 					predictions = np.array(predictions).reshape((-1, 30))
 					error_arima = np.linalg.norm(predictions[:3] - val_dataset.values[:3])/len(predictions[0])
 
-				if algo_selected == "Prophet":
-					dates = ["2007-12-" + str(i) for i in range(1, 31)]
-					predictions = []
-					for row in tqdm.tqdm(train_dataset[train_dataset.columns[-30:]].values[:3]):
-						df_prophet = pd.DataFrame(np.transpose([dates, row]))
-						df_prophet.columns = ["ds", "y"]
-						model = Prophet(daily_seasonality=True)
-						model.fit(df_prophet)
-						future = model.make_future_dataframe(periods=30)
-						forecast = model.predict(future)["yhat"].loc[30:].values
-						predictions.append(forecast)
-					predictions = np.array(predictions).reshape((-1, 30))
-					error_prophet = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
+				# if algo_selected == "Prophet":
+				# 	dates = ["2007-12-" + str(i) for i in range(1, 31)]
+				# 	predictions = []
+				# 	for row in tqdm.tqdm(train_dataset[train_dataset.columns[-30:]].values[:3]):
+				# 		df_prophet = pd.DataFrame(np.transpose([dates, row]))
+				# 		df_prophet.columns = ["ds", "y"]
+				# 		model = Prophet(daily_seasonality=True)
+				# 		model.fit(df_prophet)
+				# 		future = model.make_future_dataframe(periods=30)
+				# 		forecast = model.predict(future)["yhat"].loc[30:].values
+				# 		predictions.append(forecast)
+				# 	predictions = np.array(predictions).reshape((-1, 30))
+				# 	error_prophet = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
 
 				fig = make_subplots(rows=1, cols=1)
 				fig.add_trace(go.Scatter(x=np.arange(70), mode='lines', y=train_dataset.loc[mean_index].values, marker=dict(color="red"),name="Trained signal"),row=1, col=1)
@@ -385,19 +385,19 @@ if choice == "Category":
 					predictions = np.array(predictions).reshape((-1, 30))
 					error_arima = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
 
-				if algo_selected == "Prophet":
-					dates = ["2007-12-" + str(i) for i in range(1, 31)]
-					predictions = []
-					for row in tqdm.tqdm(train_dataset[train_dataset.columns[-30:]].values[:3]):
-						df_prophet = pd.DataFrame(np.transpose([dates, row]))
-						df_prophet.columns = ["ds", "y"]
-						model = Prophet(daily_seasonality=True)
-						model.fit(df_prophet)
-						future = model.make_future_dataframe(periods=30)
-						forecast = model.predict(future)["yhat"].loc[30:].values
-						predictions.append(forecast)
-					predictions = np.array(predictions).reshape((-1, 30))
-					error_prophet = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
+				# if algo_selected == "Prophet":
+				# 	dates = ["2007-12-" + str(i) for i in range(1, 31)]
+				# 	predictions = []
+				# 	for row in tqdm.tqdm(train_dataset[train_dataset.columns[-30:]].values[:3]):
+				# 		df_prophet = pd.DataFrame(np.transpose([dates, row]))
+				# 		df_prophet.columns = ["ds", "y"]
+				# 		model = Prophet(daily_seasonality=True)
+				# 		model.fit(df_prophet)
+				# 		future = model.make_future_dataframe(periods=30)
+				# 		forecast = model.predict(future)["yhat"].loc[30:].values
+				# 		predictions.append(forecast)
+				# 	predictions = np.array(predictions).reshape((-1, 30))
+				# 	error_prophet = np.linalg.norm(predictions[:len(item_id)] - val_dataset.values[:len(item_id)])/len(predictions[0])
 
 				fig = make_subplots(rows=len(item_id), cols=1)
 				for i in range(len(item_id)):
@@ -450,22 +450,22 @@ if choice == "Evaluation":
 		predictions = np.array(predictions).reshape((-1, 30))
 		error_prophet = np.linalg.norm(predictions[:3] - val_dataset.values[:3])/len(predictions[0])
 
-		days = range(1, 1913 + 1)
-		time_series_columns = [f'd_{i}' for i in days]
-		time_series_data = sales[time_series_columns]
-		forecast = pd.DataFrame(time_series_data.iloc[:, -28:].mean(axis=1))
-		forecast = pd.concat([forecast] * 28, axis=1)
-		forecast.columns = [f'F{i}' for i in range(1, forecast.shape[1] + 1)]
-		validation_ids = sales['id'].values
-		evaluation_ids = [i.replace('validation', 'evaluation') for i in validation_ids]
-		ids = np.concatenate([validation_ids, evaluation_ids])
-		predictions = pd.DataFrame(ids, columns=['id'])
-		forecast = pd.concat([forecast] * 2).reset_index(drop=True)
-		predictions = pd.concat([predictions, forecast], axis=1)
-		predictions.to_csv('submission.csv', index=False)
+		# days = range(1, 1913 + 1)
+		# time_series_columns = [f'd_{i}' for i in days]
+		# time_series_data = sales[time_series_columns]
+		# forecast = pd.DataFrame(time_series_data.iloc[:, -28:].mean(axis=1))
+		# forecast = pd.concat([forecast] * 28, axis=1)
+		# forecast.columns = [f'F{i}' for i in range(1, forecast.shape[1] + 1)]
+		# validation_ids = sales['id'].values
+		# evaluation_ids = [i.replace('validation', 'evaluation') for i in validation_ids]
+		# ids = np.concatenate([validation_ids, evaluation_ids])
+		# predictions = pd.DataFrame(ids, columns=['id'])
+		# forecast = pd.concat([forecast] * 2).reset_index(drop=True)
+		# predictions = pd.concat([predictions, forecast], axis=1)
+		# predictions.to_csv('submission.csv', index=False)
 
-		error = [error_avg, error_arima, error_prophet]
-		names = ["Moving average", "ARIMA", "Prophet"]
+		error = [error_avg, error_arima]
+		names = ["Moving average", "ARIMA"]
 		df = pd.DataFrame(np.transpose([error, names]))
 		df.columns = ["RMSE Loss", "Model"]
 		fig7 = px.bar(df, y="RMSE Loss", x="Model", color="Model", title="RMSE Loss vs. Model")
